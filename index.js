85645020a80d3e78ebd628579c195867faa44fda
Sudeepta Sarmah <sudeepsarmah.gmail.com>
2024-06-02 19:26:42 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('boilerplate')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('On port 3000')
+})
