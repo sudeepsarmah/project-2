968b5c7ebb51c4036a1b26b580c4ece897baba31
Sudeepta Sarmah <sudeepsarmah.gmail.com>
2024-06-02 21:28:18 +0530

    added navbar and ejs-mate

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejsMate = require('ejs-mate')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.engine('ejs', ejsMate)
 
 app.get('/', (req, res) => {
-    res.render('boilerplate')
+    res.render('pages/index')
 })
 
 
